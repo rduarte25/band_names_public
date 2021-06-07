84e0ad94f0b2d2b60772736e5053f41239587bac
RafaelDuarte <rduarte@RafaelDuarte.RafaelDuarte.com>
2021-06-07 05:56:46 -0400

    Continuación del curso  Committer: RafaelDuarte <rduarte@RafaelDuarte.RafaelDuarte.com>  On branch main  Changes to be committed: 	modified:   lib/pages/home.dart

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -16,33 +16,49 @@ class _HomePageState extends State<HomePage> {
   */
   List<Band> bands = [
     Band(id: '1', name: 'Metalica', votes: 5),
-    Band(id: '2', name: 'Queen', votes: 5),
-    Band(id: '3', name: 'Héroes del Silencio', votes: 5),
-    Band(id: '4', name: 'Bon Jovi', votes: 5),
+    Band(id: '2', name: 'Queen', votes: 2),
+    Band(id: '3', name: 'Héroes del Silencio', votes: 1),
+    Band(id: '4', name: 'Bon Jovi', votes: 10),
   ];
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: Text(
-            'Band Names',
-            style: TextStyle(color: Colors.black87),
-          ),
-          backgroundColor: Colors.white,
+      appBar: AppBar(
+        title: Text(
+          'Band Names',
+          style: TextStyle(color: Colors.black87),
         ),
-        body: ListView.builder(
-          itemBuilder: (BuildContext context, int index) =>
+        backgroundColor: Colors.white,
+        elevation: 1,
+      ),
+      body: ListView.builder(
+        itemBuilder: (BuildContext context, int index) =>
             _bandTile(bands[index]),
-          itemCount: bands.length,
-        ));
+        itemCount: bands.length,
+      ),
+      floatingActionButton: FloatingActionButton(
+        child: Icon( Icons.add ),
+        elevation: 1,
+        onPressed: () {  },
+      ),
+    );
   }
 
   ListTile _bandTile(Band band) {
     return ListTile(
-            leading: CircleAvatar(
-              child: Text(band.name!.substring(0, 2)),
-            ),
-          );
+      leading: CircleAvatar(
+        child: Text(band.name!.substring(0, 2)),
+        backgroundColor: Colors.blue[100],
+      ),
+      title: Text(band.name!),
+      trailing: Text(
+        '${band.votes!}',
+        style: TextStyle(fontSize: 20),
+      ),
+      onTap: () {
+        print(band.name!);
+      },
+    );
   }
 }
