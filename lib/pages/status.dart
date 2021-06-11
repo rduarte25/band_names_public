c3ace90ee2b0e33415516a50090b5fbc5a915e29
Rafael Duarte <elyouus94@gmail.com>
2021-06-11 18:27:12 -0400

    Continuación del curso

--- lib/pages/status.dart
+++ lib/pages/status.dart
@@ -1,13 +1,20 @@
+//Paquetes del lenguaje
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+//Paquetes propios
+import 'package:band_names/services/socket_service.dart';
 
 class StatusPage extends StatelessWidget {
   const StatusPage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
+    final socketService = Provider.of<SocketService>(context);
+
     return Scaffold(
       body: Center(
-        child: null,
+        child: Text('Hola Mundo'),
       ),
     );
   }
